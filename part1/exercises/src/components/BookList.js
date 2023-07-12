cb70ce0f61568c3cca6956154708bc4005403cf6
Sam <ssmarie14@yahoo.com>
2023-07-12 15:40:46 -0500

    updated code

--- part1/exercises/src/components/BookList.js
+++ part1/exercises/src/components/BookList.js
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "Book's I've read this month";
+   let book1 = "https://m.media-amazon.com/images/I/511hkXlSvLL._SL500_.jpg";
+   let book2 = "https://m.media-amazon.com/images/I/61Yu7zVGD0L._SY425_.jpg";
+   let book3 = "https://m.media-amazon.com/images/I/71S5PVQRdOL._SY425_.jpg";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="You're Not Dying, You're Just Waking Up" />
+         <img src={book2} alt="The ChatGPT Millionaire" />
+         <img src={book3} alt="The People We Keep" />
       </div>      
    );
 }
